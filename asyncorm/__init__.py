f2f6dbac866b673bed9deb27af0d20b2db8e22cd
monobot <monobot.soft@gmail.com>
2017-03-02 12:00:04 +0000

    small changes on documentation

--- asyncorm/__init__.py
+++ asyncorm/__init__.py
@@ -4,3 +4,8 @@ from .fields import *
 from .exceptions import *
 from .database import PostgresManager
 from .application import configure_orm
+
+__doc__ = '''
+asyncorm is a fully asynchronous ORM library
+'''
+__version__ = '0.0.1'
