716034a1af0ee3d6e99741159b136a83ba5a644c
monobot <monobot.soft@gmail.com>
2017-03-19 17:26:02 +0000

    new pypi release

--- asyncorm/__init__.py
+++ asyncorm/__init__.py
@@ -1,12 +1,12 @@
-from .model import *
-from .fields import *
-
-from .exceptions import *
-from .database import PostgresManager
 from .application import configure_orm, orm_app
+from .database import PostgresManager
+from .exceptions import *
+from .fields import *
 from .log import logger
+from .model import *
 
 __doc__ = '''
 asyncorm is a fully asynchronous ORM library
+inspired by django's own ORM
 '''
-__version__ = '0.0.2'
+__version__ = '0.0.3'
