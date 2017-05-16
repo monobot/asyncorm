109d6066f9d19aa9492c2e8e47a4d241eae93210
monobot <monobot.soft@gmail.com>
2017-05-16 22:16:47 +0100

    edit travis configfile

--- tests/__main__.py
+++ tests/__main__.py
@@ -8,8 +8,8 @@ from asyncorm.application import configure_orm
 db_config = {
     'database': 'asyncorm',
     'host': 'localhost',
-    'user': 'sanicdbuser',
-    'password': 'sanicDbPass',
+    'user': 'postgres',
+    'password': '',
 }
 orm_app = configure_orm({
     'db_config': db_config,
