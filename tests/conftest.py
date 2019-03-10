33624c2caa5644ddd485644cc0d1d27638239aef
monobot <monobot.soft@gmail.com>
2019-03-10 12:01:09 +0100

    fix conflicts

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,14 @@
+import asyncio
+import os
 import pytest
+
+from tests.app_1.models import Author, Book
 
 
 def pytest_sessionstart(session):
     """ before session.main() is called. """
     print("yes")
+
 
 def pytest_sessionfinish(session, exitstatus):
     """ whole test run finishes. """
